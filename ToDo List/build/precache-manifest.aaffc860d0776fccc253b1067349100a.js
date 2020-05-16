899a9c81d41f0d78f09ca04ec748e274f984388f
Tenezar10037 <44014041+Tenezar10037@users.noreply.github.com>
2020-05-16 13:03:37 +0530

    Add files via upload

---
+++ ToDo List/build/precache-manifest.aaffc860d0776fccc253b1067349100a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac22731ba6891e736368aea7df24bbb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531b411e6954ef914cb3",
+    "url": "/static/css/main.d3b2848a.chunk.css"
+  },
+  {
+    "revision": "c92ba26116b2c6bc0e73",
+    "url": "/static/js/2.c79ae48e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c79ae48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531b411e6954ef914cb3",
+    "url": "/static/js/main.677462ed.chunk.js"
+  },
+  {
+    "revision": "38cd6332ffceca4ced93",
+    "url": "/static/js/runtime-main.b418b0f3.js"
+  }
+]);
